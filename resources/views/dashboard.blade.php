8c0c31e3badb24ddced4d4253a11dec99aa536eb
AlexRomanR <thealex6969@gmail.com>
2024-06-22 00:09:53 -0400

    Dash seed api

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -497,6 +497,70 @@
 
 
 
+                                                @php
+                                                // Eager load 'calificacion_empleado' relationship and filter the results directly in the query
+                                                $empleado = Auth::user()->empleado()->with(['calificacion_empleado' => function($query) {
+                                                    $query->where('mes', '05')->where('anio', '2024');
+                                                }])->first();
+                                            
+                                                $calificacion = $empleado->calificacion_empleado->first();
+                                                $puntaje = $calificacion ? $calificacion->puntaje : null;
+                                            @endphp
+
+                                                @if ($puntaje !== null)
+                                                    Puntaje obtenido: {{ $puntaje }}
+
+                                                    @php
+                                                        // Calcular la cantidad de estrellas a mostrar
+                                                        $estrellas = min($puntaje / 20, 5); // Cada 20 puntos es una estrella, con un máximo de 5 estrellas
+
+                                                        // Calcular el número de estrellas completas y medias estrellas
+                                                        $estrellasCompletas = floor($estrellas);
+                                                        $mediaEstrella = $estrellas - $estrellasCompletas;
+                                                        $estrellasVacias = 5 - ceil($estrellas);
+                                                    @endphp
+
+                                                    <div class="flex">
+                                                        {{-- Estrellas llenas --}}
+                                                        @for ($i = 1; $i <= $estrellasCompletas; $i++)
+                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                viewBox="0 0 20 20" fill="currentColor"
+                                                                class="h-5 w-5 text-yellow-400">
+                                                                <path
+                                                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                                                </path>
+                                                            </svg>
+                                                        @endfor
+
+                                                        {{-- Media estrella --}}
+                                                        @if ($mediaEstrella > 0)
+                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                viewBox="0 0 20 20" fill="currentColor"
+                                                                class="h-5 w-5 text-yellow-400">
+                                                                <path
+                                                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                                                </path>
+                                                            </svg>
+                                                        @endif
+
+                                                        {{-- Estrellas vacías --}}
+                                                        @for ($i = 1; $i <= $estrellasVacias; $i++)
+                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                viewBox="0 0 20 20" fill="currentColor"
+                                                                class="h-5 w-5 text-gray-300">
+                                                                <path
+                                                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                                                </path>
+                                                            </svg>
+                                                        @endfor
+                                                    </div>
+                                                @else
+                                                    No se encontró calificación para el mes 05 y año 2024.
+                                                @endif
+
+
+
+
 
 
 
